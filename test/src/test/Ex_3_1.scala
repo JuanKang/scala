03c775677026a79cda24eeb20476acc42ce8d56b
juan Kang <kangkss2@gmail.com>
2017-03-20 01:20:19 +0900

    Signed-off-by: juan Kang <kangkss2@gmail.com>

---
+++ test/src/test/Ex_3_1.scala
@@ -0,0 +1,13 @@
+package test
+
+object Ex_3_1 {
+  var x = 20
+  if (x > 15 ) {
+    println("크다 ")
+    println("ㅌㅌㅌ")
+  } else if (x <15) {
+    println("작다")
+  } else {
+    println("멀까")
+  }
+}
